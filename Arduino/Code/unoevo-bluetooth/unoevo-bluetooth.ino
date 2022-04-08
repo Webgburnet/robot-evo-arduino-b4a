8ff0c1653fa0b1eb892501e4977ca19ad25edc21
Gregoire BURNET <44721323+Webgburnet@users.noreply.github.com>
2022-04-08 11:40:02 +0200

    Update unoevo-bluetooth.ino

--- Arduino/Code/unoevo-bluetooth/unoevo-bluetooth.ino
+++ Arduino/Code/unoevo-bluetooth/unoevo-bluetooth.ino
@@ -14,32 +14,33 @@ SoftwareSerial mavoieserie(RX_pin, TX_pin); // (RX, TX) (pin Rx BT, pin Tx BT)
 
 int Tension=0;
 int Pad;
+int Clav;
 
 Servo Servo_G;
 Servo Servo_D;
+
+const int MIN=500; 
+const int MAX=1000; 
 
 int Pos_D, Pos_G;
 
 void setup()
 {
-  
+  Serial.println("Debut setup");
   pinMode( LedPin , OUTPUT);
   pinMode( BuzzerPin , OUTPUT);
   pinMode(switchPin, INPUT);
+  pinMode (Servo_DPin,OUTPUT);
+  pinMode (Servo_GPin,OUTPUT);
   Serial.begin(115200);
   mavoieserie.begin(115200);
-  Serial.println("Debut setup");
-  Servo_G.attach(Servo_GPin, 0, 1000);
-  Servo_D.attach(Servo_DPin, 0, 1000);
   Pos_G=Servo_G.read();
   Serial.println(Pos_G);
   Pos_D=Servo_D.read();
   Serial.println(Pos_D);
-  Servo_G.write(86);
-  Servo_D.write(86);
   delay (1000);
+  Serial.print("Pad=");
   Serial.println(Pad);
-
   digitalWrite(LedPin , LOW);
   digitalWrite(BuzzerPin , LOW);
   Serial.println("Fin setup");
@@ -54,39 +55,47 @@ void loop()
     if (mavoieserie.available()>0) {
           //Serial.println("Telephone");
           Pad=mavoieserie.read();
+          Serial.print("Pad=");
           Serial.println(Pad);
       if (Pad == 'Z' )
       {
         //Avancer();
         Serial.print("Avancer");
         Serial.println();
-        Servo_G.write(1000);
-        Servo_D.write(500);
+        Servo_G.attach(Servo_GPin);
+        Servo_G.write(MAX);
+        Servo_D.attach(Servo_DPin);
+        Servo_D.write(MIN);
       }
-      
       if (Pad=='Q')
       {
         //Gauche();
-        Serial.print("Reculer");
+        Serial.print("Droite");
         Serial.println();
-        Servo_G.write(500);
-        Servo_D.write(500);
+        Servo_G.attach(Servo_GPin);
+        Servo_G.write(MAX);
+        Servo_D.attach(Servo_DPin);
+        Servo_D.write(MAX);
       }
       if (Pad =='S')
       {
         //Reculer();
         Serial.print("Reculer");
         Serial.println();
-        Servo_G.write(500);
-        Servo_D.write(1000);
+        Servo_G.attach(Servo_GPin);
+        Servo_G.write(MIN);
+        Servo_D.attach(Servo_DPin);
+        Servo_D.write(MAX);
       }
       if (Pad=='D')
       {
         //Droite();
-        Serial.print("Droite");
+        Serial.print("Gauche");
         Serial.println();
-        Servo_G.write(1000);
-        Servo_D.write(1000);
+        Servo_G.attach(Servo_GPin);
+        Servo_G.write(MIN);
+        Servo_D.attach(Servo_DPin);
+        Servo_D.write(MIN);
       }
        if (Pad == 'B' ) //Start
       {
@@ -94,8 +103,8 @@ void loop()
         Serial.print("STOP");
         Serial.println();
         //Stop();
-        Servo_G.write(86);
-        Servo_D.write(86);
+        Servo_G.detach();
+        Servo_D.detach();
       }
       if (Pad=='L') //Croix
       {
